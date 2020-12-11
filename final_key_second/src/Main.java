04cff40f76f5ed85fb97dbf3b494c68a3cf2ea73
Halil İbrahim KOÇAK <halibrahim.kocak@gmail.com>
2020-12-11 14:04:48 +0300

    Update Main.java

--- final_key_second/src/Main.java
+++ final_key_second/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         DataBase dataBase = new DataBase();
         dataBase.baglantıKur("hllbr", "123456");
+        //Source = Mustafa Murat COŞKUN / UDEMY referans alınarak geliştirilmiştir..
     }
     
 }
